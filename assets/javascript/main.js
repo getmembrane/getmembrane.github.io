34f6588c97501d12df63c835b609bf0f8b3dee9b
Kristoffer Darj <kristoffer.darj@gmail.com>
2016-12-09 23:35:38 +0100

    Rolling cube

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,2 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    var cube = document.querySelector('.cube');
+    var offset = 0;
+    var width = cube.offsetWidth;
+
+    cube.addEventListener('animationiteration',function() {
+        cube.style.left = parseInt(offset + width) + 'px';
+        offset = parseInt(offset + width);
+    });
 });
